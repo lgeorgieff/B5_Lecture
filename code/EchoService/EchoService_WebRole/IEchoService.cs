7b97cc1111653c68a80f6200bb0588c77fdae9e2
Lukas Georgieff <lukas.georgieff@hotmail.com>
2013-12-01 12:25:42 +0100

    initial commit
    created the basic service logic for the EchoService

---
+++ code/EchoService/EchoService_WebRole/IEchoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EchoService_WebRole
+{
+    [ServiceContract]
+    public interface IEchoService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "echo-get/{message}")]
+        string EchoGet(string message);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "echo-post")]
+        string EchoPost(string message);
+
+    }
+}
---
+++ code/EchoService/EchoService_WebRole/Service1.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EchoService_WebRole
+{
+    public class EchoService : IEchoService
+    {
+        public string EchoGet(string message)
+        {
+            return "[" + DateTime.Now.ToString("dd.MM.yyyy") + "] EchoGet(): " + message;
+        }
+
+        public string EchoPost(string message)
+        {
+            return "[" + DateTime.Now.ToString("dd.MM.yyyy") + "] EchoPost(): " + message;
+        }
+    }
+}

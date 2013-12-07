820a3809de591178f39b1eacb3c074a9b77ce786
Lukas Georgieff <lukas.georgieff@hotmail.com>
2013-12-07 21:52:32 +0100

    added service bus usage

--- code/EchoService/EchoService_WebRole/EchoService.svc.cs
+++ code/EchoService/EchoService_WebRole/EchoService.svc.cs
@@ -10,13 +10,18 @@ namespace EchoService_WebRole
 {
     public class EchoService : IEchoService
     {
+        // The name of your queue
+        public const string QUEUE_NAME = "my-message-queue";
+
         public string EchoGet(string message)
         {
+            (new ServiceBusQueue(QUEUE_NAME, false)).SendMessage(message);
             return "[" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + "] EchoGet(): " + message;
         }
 
         public string EchoPost(string message)
         {
+            (new ServiceBusQueue(QUEUE_NAME, false)).SendMessage(message);
             return "[" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + "] EchoPost(): " + message;
         }
     }
---
+++ code/EchoService/EchoService_WebRole/ServiceBusQueue.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace EchoService_WebRole
+{
+    public class ServiceBusQueue
+    {        
+        private NamespaceManager namespaceManager = null;
+        private QueueDescription queueDescription = null;
+        private string name = null;
+        private QueueClient client = null;
+
+        public ServiceBusQueue(string name, bool reset)
+        {
+            this.namespaceManager = NamespaceManager.Create();
+            if (reset && this.namespaceManager.QueueExists(name))
+            {
+                this.namespaceManager.DeleteQueue(name);
+                this.queueDescription = this.namespaceManager.CreateQueue(name);
+            }
+            else
+            {
+                this.queueDescription = this.namespaceManager.GetQueue(name);
+            }
+            this.name = name;
+            this.client = QueueClient.CreateFromConnectionString(WebConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"], this.name);
+        }
+
+        public long SendMessage(object messageBody)
+        {
+            BrokeredMessage message = new BrokeredMessage(messageBody);
+            message.MessageId = Guid.NewGuid().ToString();
+            this.client.Send(message);
+            return this.queueDescription.MessageCount;
+        }
+    }
+}
